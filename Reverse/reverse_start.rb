dbf455c94982caece94e77b8a9c1e02d4ce62fc8
Silvia Rocha <silviadprocha@gmail.com>
2020-02-01 14:41:34 +0900

    finished reverse exercise 7 kyu

---
+++ Reverse/reverse_start.rb
@@ -0,0 +1,8 @@
+# Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+def reverse_words(str)
+
+end
+
+puts reverse_words("This is an example!")
+
+# "sihT si na !elpmaxe"
