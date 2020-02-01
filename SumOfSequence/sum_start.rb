b06879b2a94727723c1862926e0c89bcee4f4c78
Silvia Rocha <silviadprocha@gmail.com>
2020-02-01 14:50:24 +0900

    add start exercise sum of sequence 7 kyu

---
+++ SumOfSequence/sum_start.rb
@@ -0,0 +1,8 @@
+# Your task is to make function, which returns the sum of a sequence of integers.
+# The sequence is defined by 3 non-negative values: begin, end, step.
+# If begin value is greater than the end, function should returns 0
+def sequence_sum(begin_number, end_number, step)
+  #your code here
+end
+
+puts sequence_sum(2,6,2)
