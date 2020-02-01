c052e688be200b00a13030d8bc33d6a8f9d26a7b
Silvia Rocha <silviadprocha@gmail.com>
2020-02-01 14:38:29 +0900

    finished exercise 7 kyu

--- SortArray/sort_array_finished.rb
+++ SortArray/sort_array_finished.rb
@@ -1,7 +1,7 @@
 # Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
 # All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
 def sort_by_length(arr)
-  arr
+  arr.sort_by { |str| str.length } # or arr.sort_by(&:length)
 end
 
 puts sort_by_length(["Telescopes", "Glasses", "Eyes", "Monocles"])
