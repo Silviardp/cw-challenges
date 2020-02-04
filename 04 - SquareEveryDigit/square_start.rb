00f3211bb2dc469beb76d7838574525015ba6672
Silvia Rocha <silviadprocha@gmail.com>
2020-02-04 15:44:10 +0900

    start 4th exercise

---
+++ 04 - SquareEveryDigit/square_start.rb
@@ -0,0 +1,14 @@
+=begin
+Welcome. In this kata, you are asked to square every digit of a number.
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+Note: The function accepts an integer and returns an integer
+=end
+
+def square_digits num
+  # code goes here
+end
+
+puts square_digits(3212);
+# 9414
+puts square_digits(2112);
+# 4114
