f2693d6c1fd65c86545af87f0ac2732276f0ac14
Silvia Rocha <silviadprocha@gmail.com>
2020-03-08 09:19:49 +0900

    initiate ex growth population 7kyu

---
+++ 09 - GrowthPopulation/growth_start.rb
@@ -0,0 +1,12 @@
+=begin
+  In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
+=end
+
+def nb_year(p0, percent, aug, p)
+
+end
+
+puts nb_year(1500, 5, 100, 5000)
+#15
+puts nb_year(150000, 2.5, 10000, 2000000)
+#10
