f9c9cdff1d4ec69c6931e4d4af49857d68e50011
Silvia Rocha <silviadprocha@gmail.com>
2020-05-11 11:34:11 +0900

    add binary addition 7 kyu exercise

---
+++ 19 - Binary Addition/binary_addition_start.rb
@@ -0,0 +1,19 @@
+=begin
+Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+The binary number returned should be a string.
+=end
+
+def add_binary(a,b)
+  #your code here
+end
+
+puts add_binary(1,1)
+#10
+puts add_binary(0,1)
+#1
+puts add_binary(1,0)
+#1
+puts add_binary(2,2)
+#100
+puts add_binary(51,12)
+#111111
